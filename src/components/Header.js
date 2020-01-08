87a71f04955362f705606f40068172eeceb4911e
Sobenkov <Sobenkov95@yandex.ru>
2020-01-08 14:48:31 +0300

    ReactSocial

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+  	<header className="header">
+      <img src="http://dummyimage.com/100x100/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image"/>
+   </header>);
+}
+
+export default Header;
